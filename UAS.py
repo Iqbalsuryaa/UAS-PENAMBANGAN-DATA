819fb3062376129ff9288f6274f809803d29de67
Mohammad Iqbal Surya Ramadhan <112038977+Iqbalsuryaa@users.noreply.github.com>
2023-06-13 20:06:55 +0700

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import streamlit as st
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+from streamlit_option_menu import option_menu
+
+#navigasi sidebar
+# horizontal menu
+selected2 = option_menu(None, ["Data", "Procecing data", "Modelling", 'Implementasi'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+#halaman Data
+if (selected2 == 'Data') :
+    st.title('deskripsi data')
+
+    st.write("Ini adalah contoh data yang tersedia dalam aplikasi Streamlit.")
+    st.write("Data ini berisikan informasi mengenai pria dan wanita, yang bertujuan untuk mengetahui memiliki rambut yang panjang atau tidak.")
+    st.write("Data ini diambil dari kaggle")
+    st.write("Data ini merupakan type data Numerik")
+    data = pd.read_csv('gender_classification_v7_oke.csv')
+    st.write(data)
+
+
+
+#halaman procecing data 
+if (selected2 == 'Procecing data') :
+    st.title('Procecing Data')
+
+    st.write("saya menggunakan procecing data SKALASI STANDAR ")
+    st.write("Dengan hasil procecing data")
+    data = pd.read_csv('preprocessed_data.csv')
+    st.write(data)
+
+
+#halaman modelling
+if (selected2 == 'Modelling'):
+    st.title('Modelling')
+
+    pilih = st.radio('Pilih', ('Naive Bayes', 'Decision Tree', 'KNN', 'ANN'))
+
+    if (pilih == 'Naive Bayes'):
+        st.title(' Nilai Akurasi 100%')
+    elif (pilih == 'Decision Tree'):
+        st.title(' Nilai Akurasi 100%')
+    elif (pilih == 'KNN'):
+        st.title(' Nilai Akurasi 100%')
+    elif (pilih == 'ANN'):
+        st.title(' Nilai Akurasi 100%')
+
+
+#halaman implementasi
+# Load the saved model
+if (selected2 == 'Implementasi'):
+    st.title('Implementasi')
+
+
+    
