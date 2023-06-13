0f06a8934137896b18f83f1d061288528be1ffaf
Mohammad Iqbal Surya Ramadhan <112038977+Iqbalsuryaa@users.noreply.github.com>
2023-06-13 22:11:55 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -56,62 +56,7 @@ if (selected2 == 'Modelling'):
 if (selected2 == 'Implementasi'):
     st.title('Implementasi')
     
-    model_filename = 'loan.pkl'
-    with open(model_filename, 'rb') as file:
-        model = pickle.load(file)
-
-    # Create a function to preprocess the input data
-import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
-
-# Memuat data yang telah diproses sebelumnya
-df_std = pd.read_csv('preprocessed_data.csv')
-
-# Menyiapkan data untuk pemodelan
-X = df_std[['gender', 'forehead_width', 'forehead_height_cm', 'nose_long', 'lips_thin', 'distance_nose_to_lip_long']].values
-y = df_std['long_hair'].values
-
-# Menstandarisasi fitur-fitur
-sc = StandardScaler()
-X = sc.fit_transform(X)
-
-# Melatih model klasifikasi Gaussian Naive Bayes
-classifier = GaussianNB()
-classifier.fit(X, y)
-
-# Fungsi untuk memprediksi rambut panjang berdasarkan input pengguna
-def predict_long_hair(gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long):
-    input_data = [gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long]
-    input_data = sc.transform([input_data])
-    predicted_long_hair = classifier.predict(input_data)
-    return predicted_long_hair[0]
-
-# Aplikasi web menggunakan Streamlit
-def main():
-    st.title("Prediksi Panjang Rambut")
-    st.write("Masukkan informasi berikut untuk memprediksi apakah seseorang memiliki rambut panjang atau tidak:")
-
-    gender = st.selectbox("Jenis Kelamin", ["Perempuan", "Laki-laki"])
-    forehead_width = st.slider("Lebar Dahi", min_value=0.0, max_value=1.0, step=0.01)
-    forehead_height = st.slider("Tinggi Dahi (cm)", min_value=0.0, max_value=1.0, step=0.01)
-    nose_long = st.slider("Panjang Hidung", min_value=0.0, max_value=1.0, step=0.01)
-    lips_thin = st.slider("Ketipisan Bibir", min_value=0.0, max_value=1.0, step=0.01)
-    distance_nose_to_lip_long = st.slider("Jarak dari Hidung ke Bibir", min_value=0.0, max_value=1.0, step=0.01)
-
-    if st.button("Prediksi"):
-        predicted_hair_length = predict_long_hair(gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long)
-        if predicted_hair_length == 1:
-            st.write("Orang tersebut diprediksi memiliki rambut panjang.")
-        else:
-            st.write("Orang tersebut diprediksi tidak memiliki rambut panjang.")
-
-if __name__ == '__main__':
-    main()
-
-
+    
 
     
 
